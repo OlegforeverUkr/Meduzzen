4e4ea2b95547f458d618e52d58ed8024385d13ab
Oleg <shlyapin8848@gmail.com>
2024-04-11 14:07:23 +0300

    fix Dockerfile and main

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ COPY ./app /code/app
 COPY ./tests /code/tests
 
 #
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+CMD ["python", "-m", "app.main"]
--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ setup_cors(app=app)
 app.include_router(router=router)
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host=settings.HOST, port=settings.PORT, reload=settings.RELOAD)
+    uvicorn.run(app="app.main:app", host=settings.HOST, port=settings.PORT, reload=settings.RELOAD)
