188fad89ac1b7e99450a9758a4f04eeedf2abbe1
Oleg <shlyapin8848@gmail.com>
2024-04-13 15:09:08 +0300

    Changed Readme and added logging

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from app.routers.routers import router
 from app.core.config import settings
 from app.core.middleware import setup_cors
 import uvicorn
+import logging
 
 app = FastAPI()
 setup_cors(app=app)
 app.include_router(router=router)
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     uvicorn.run(app="app.main:app", host=settings.HOST, port=settings.PORT, reload=settings.RELOAD)
