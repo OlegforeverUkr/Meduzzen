df67dbe61c2d83f961dbfdf357fa9d770bf1c38c
Oleg <shlyapin8848@gmail.com>
2024-04-15 13:06:38 +0300

    fix Base model

--- app/db/base_model.py
+++ app/db/base_model.py
@@ -1,3 +1,8 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, Column, DateTime, func
+from sqlalchemy.orm import DeclarativeBase
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
--- app/db/models.py
+++ app/db/models.py
@@ -1,11 +1,10 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, String, Boolean
 from app.db.base_model import Base
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
