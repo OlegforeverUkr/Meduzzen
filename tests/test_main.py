4e3228dc8657613a03be6f281ee409eab3afe530
Oleg <shlyapin8848@gmail.com>
2024-04-16 12:50:51 +0300

    added router and crud but without tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from app.schemas.users import UserCreateSchema
 
 
 @pytest.mark.asyncio
-async def test_health_check():
+async def test_read_main():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
